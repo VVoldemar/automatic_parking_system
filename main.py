632c96fcfd32d6303ee4a8db2b92999076d83459
StepanEremkin <stepa.bike@gmail.com>
2025-01-27 19:12:15 +0300

    add SocketServer

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from Camera import Camera
 from WebServer import start_web_server
+from SocketServer import SocketServer
 
 
 def main():
     camera = Camera()
     flask_thread = start_web_server(camera)
+
+    socket = SocketServer()
+
     flask_thread.join()
 
 
